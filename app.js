e7c2f705132c3453407af264c61691b55df05c84
csmel <melsakas@gmail.com>
2018-06-15 10:14:46 -0400

    Updated version with SCSS, and the app displays proportionaly

--- app.js
+++ app.js
@@ -7,43 +7,52 @@ new Vue({
         //loser: 'Lost',
         //surprise:'https://media.giphy.com/media/x4O0fjpQfoBZS/giphy.gif',
         resetVal: '',
-
+        start: true,
+        replay: false,
     },
 
     methods: {
         randomNum: function () {
 
-            if (this.playerNum > 0) {
-                this.computerNum = Math.floor(Math.random() * (10 - 1)) + 1;
-                return this.computerNum ;
+            if (this.playerNum > 0 && this.playerNum < 11) {
+                this.computerNum = Math.floor(Math.random() * (11 - 1)) + 1;
+                return [this.computerNum, this.playButton()];
             }
+
             else {
                 return this.resetVal;
             }
-        },
-        resetButton: function() {
-            if(this.computerNum !== this.playerNum)
 
-                return [this.computerNum = '' , this.playerNum = '',];
+        },
+        playButton: function () {
+            if (this.start === true || this.replay === false) {
+                return [this.start = false, this.replay = true]
+            }
+        },
+
+        resetButton: function () {
+
+            if (this.computerNum !== this.playerNum && this.start === false && this.replay === true)
+
+                return [this.computerNum = '', this.playerNum = '', this.start = true, this.replay = false];
         }
     },
 
     computed: {
-
-
         determin: function () {
-            if ( this.playerNum === '0' || this.playerNum < 0){
+            if (this.playerNum === '0' || this.playerNum < 0) {
                 return 'https://media.giphy.com/media/3og0IJXQEKwIdIEYpy/giphy.gif';//try again
             }
             else if (this.computerNum === '' || this.playerNum === '') {
-                return 'https://media.giphy.com/media/zaezT79s3Ng7C/giphy.gif' ;
+                return 'https://media.giphy.com/media/zaezT79s3Ng7C/giphy.gif'; //start
             }
-            else if (this.computerNum === this.playerNum) {
+            else if (this.computerNum == this.playerNum) {
                 return 'https://media.giphy.com/media/x4O0fjpQfoBZS/giphy.gif'; //balloon
             }
-            else if(this.computerNum !== this.playerNum) {
+            else if (this.computerNum !== this.playerNum) {
                 return 'https://media.giphy.com/media/d2W7eZX5z62ziqdi/giphy.gif'; //fail
             }
+
 
         },
 
